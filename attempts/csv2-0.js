9350e7e969d611e3ab4fb6c79c0b36b820ada0fa
clement <maboiteaspam@wanadoo.fr>
2015-12-09 20:40:25 +0800

    some

--- attempts/csv2-0.js
+++ attempts/csv2-0.js
@@ -5,15 +5,26 @@ var start = new Date();
 var line = 0;
 var flower = require('flower')
 var through2 = require('through2')
+var csv2 = require('csv2')();
+csv2.resume()
 
 require('fs')
   .createReadStream('../worldcitiespop.txt')
-  .pipe(require('csv2')())
-  .pipe(through2.obj())
-  .pipe(through2.obj())
-  .pipe(through2.obj())
-  .pipe(through2.obj())
-  .pipe(through2.obj())
+  .pipe((function () {
+    var t = '';
+    return flower(function (chunk, enc, cb) {
+      t += chunk.toString('utf-8');
+      var k = t.split(/\n/);
+      var p = 0;
+      for(var i=0;i<k.length-1;i++) {
+        this.push(k[i]+'\n')
+        p+=k[i].length
+      }
+      t = t.substr(p);
+      cb(null)
+    });
+  })())
+  .pipe(csv2)
   .on('data', function (){
     line++;
     line%30000===0 && console.log('%s lines', line)
